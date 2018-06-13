42f60d861c733f20b2080cc58971c73b6c9abbdf
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-06-13 10:29:04 +0400

    + gui .py
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- wxGUI.py
+++ wxGUI.py
@@ -1,4 +1,4 @@
-# console calculator with infix syntax
+# calculator with infix syntax with colorized GUI editor (wxPython/Scintilla)
 #
 # was written with Python and PLY library as a sample for Quora blogpost:
 # http://ponyatov.quora.com/Text-data-formats-Parsing-with-Python-and-PLY-library
