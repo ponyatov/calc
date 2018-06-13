e86c741f06bdfddddb25aba6500ea2a13b39a4db
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-06-13 10:27:54 +0400

    +console .py

---
+++ console.py
@@ -0,0 +1,7 @@
+# console calculator with infix syntax
+# was written with Python and PLY library as a sample for Quora blogpost:
+# http://ponyatov.quora.com/Text-data-formats-Parsing-with-Python-and-PLY-library
+
+import ply.lex  as lex
+import ply.yacc as yacc
+
